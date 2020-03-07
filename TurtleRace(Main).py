3b8c8d9fcfdc3320087266ae2e9756a766e11d2b
Martín <martingomval@gmail.com>
2020-03-07 14:24:18 +0100

    End of the TurtleRace

--- TurtleRace(Main).py
+++ TurtleRace(Main).py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuit():
     turtles = []
@@ -11,7 +12,7 @@ class Circuit():
         self.__screen.setup(widht, height)
         self.__screen.bgcolor('lightgray')
         self.__startLine = -(widht / 2) + 20
-        self.__finshLine = widht / 2 - 20
+        self.__finishLine = +(widht / 2) - 20
         
         self.__Turtles()
         
@@ -25,11 +26,18 @@ class Circuit():
             
             self.turtles.append(newTurtle)
             
-    
-        
-        
-        
-        
+    def compite(self):  
+        existWinner = False
+        while not existWinner:
+            for turtle in self.turtles:
+                progress = random.randint(1, 6) # Creamos el progreso random
+                turtle.forward(progress)
+                
+                if turtle.position()[0] >= self.__finishLine:
+                    existWinner = True
+                    print("{} turtle has won!".format(turtle.color()[0]))
+                    break
         
 if __name__ == "__main__":
     circuit = Circuit(640, 480)
+    circuit.compite()
